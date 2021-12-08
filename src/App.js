2db04a6b922907606e43add8786f875fff4b3991
undefined name, please set in repo config <somewhere@example.com>
2021-12-08 12:18:53 +0930

    introducing the drum and cymbol objects to use with the pad
    renaming some of the classes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import './App.css';
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
+
+// basing this stuff on the demo here:
+// https://codepen.io/freeCodeCamp/pen/MJyNMd
+const cymbols = [
+  {
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: "highHat",
+    url: "https://upload.wikimedia.org/wikipedia/commons/3/3d/Hi_hat_open.ogg"
+  }
+]
+
+const drums = [
+  keyCode: 65,
+  keyTrigger: 'Q',
+  id: "snareDrum",
+  url: "https://soundcamp.org/sounds/381/snare/A/subtle-reverb-snare-drum-sound-a-key-01-Kb6.ogg"
+
+]
 
 class App extends React.Component {
   constructor(props) {
@@ -26,27 +45,35 @@ class App extends React.Component {
   }
 }
 
-function Pad() {
+class Pad extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todo: true
+    }
+  };
+  render() {
   return (
-    <div className="container bg-secondary" id="pad">
-      <div className="row" id="topOfKit">
-        <div className="col-sm-4" id="snareDrum">
-          <h4>snareDrum</h4>
+      <div className="container bg-secondary" id="pad">
+        <div className="row" id="cymbols">
+          <div className="col-sm-6" id="highHat">
+            <h4>highHat</h4>
+          </div>
+          <div className="col-sm-6" id="cymbol">
+            <h4>cymbol</h4>
+          </div>
         </div>
-        <div className="col-sm-4" id="highHat">
-          <h4>highHat</h4>
-        </div>
-        <div className="col-sm-4" id="cymbol">
-          <h4>cymbol</h4>
+        <div className="row" id="drums">
+          <div className="col-sm-6" id="snareDrum">
+            <h4>snareDrum</h4>
+          </div>
+          <div className="col-sm-6" id="bassDrum">
+            <h4>bassDrum</h4>
+          </div>
         </div>
       </div>
-      <div className="row" id="bottomOfKit">
-        <div className="col-sm-12" id="bassDrum">
-          <h4>bassDrum</h4>
-        </div>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 function Controls() {
